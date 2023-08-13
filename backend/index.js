c1da625f55d8f666bf45e1dbf7b2ed6a446b74e4
Ameya Shahu <ameyashahu@gmail.com>
2023-08-13 11:02:00 -0700

    proxy server created

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,34 @@
-console.log("hello World")
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+
+
+const HOST = "localhost";
+const API_SERVICE_URL = "https://json.freeastrologyapi.com";
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+
+// enable cors
+app.use(cors({
+    origin: '*'
+}));
+
+app.use('/api', createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/api`]: '',
+    },
+    headers: {"x-api-key": process.env.API_KEY}
+ }));
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`${new Date()} --  Backend server is running on port no ${PORT}`)
+});
